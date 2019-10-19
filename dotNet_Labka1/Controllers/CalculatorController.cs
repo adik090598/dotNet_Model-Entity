52b7a1c93fee893d67c08e9b85aef06eda89f2eb
adik090598 <adik090598@mail.ru>
2019-10-19 17:42:22 +0600

    first commit

---
+++ dotNet_Labka1/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNet_Labka2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult PlusTen(int number)
+        {
+            ViewData["number"] = number;
+            ViewData["result"] = number + 10;
+
+            return View();
+        }
+    }
+}
